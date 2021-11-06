021e7f76c380512e7209a44b44a51efa85f9c16b
+ <+>
2021-11-06 11:06:06 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+function EyesOnMe(){
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    function handleFocus(){
+        console.log("Good!")
+    }
+    return(
+        <div>
+        <button onFocus={handleFocus}onBlur={handleBlur} >Eyes On Me</button>
+        </div>
+   
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react';
+function Keypad(){
+    function handleChange(){
+        console.log("Entering password...")
+    }
+    return(
+    <div>
+        <input type = "password" onChange={handleChange}/>
+    </div>
+    )
+}
+export default Keypad;
